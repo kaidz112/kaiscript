c5e56d2aae390fed9b43cf73a7a607dfea5abb67
Kaidz112 <myduyen5665@gmail.com>
2026-02-18 12:33:43 +0700

    Add imports for time, os, requests, and rich
    Added multiple imports for various libraries.

--- gay.py
+++ gay.py
@@ -1,1 +1,18 @@
+import time
+from time import strftime
+import os
+import sys
+import requests
+import json
+from time import sleep
+from datetime import datetime, timedelta
+import base64
+import requests
+import os
+import subprocess
+from pystyle import Colors, Colorate
+from rich.console import Console
+from rich.panel import Panel
+from rich.console import Console
+from rich.text import Text
 print('có con cặc')
